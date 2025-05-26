d48b5ddec7c80bdf56664e81658c5184464ce2bc
polinestasiw <poline.stasiw@escola.pr.gov.br>
2025-05-26 17:22:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'sertanejo',
+    'Quem e Jorge e Matheus?',
+    ' Jorge & Mateus é uma dupla brasileira de música sertaneja, formada em Itumbiara em 2005 pelos cantores Jorge Barcelos e Mateus Liduário.'
+)
+
+criaCartao(
+    'sertanejo',
+    'Qual a musica mais famosa deles?',
+    'O que e que tem;'
+)
+
+criaCartao(
+    'sertanejo',
+    'Quem e Henrique & Juliano?',
+    'Henrique & Juliano é uma dupla sertaneja formada pelos irmãos Ricelly Henrique Tavares Reis e Edson Alves dos Reis Junior, ambos nascidos na cidade de Palmeirópolis no interior do estado do Tocantins.'
+)
+
+criaCartao(
+    'sertanejo',
+    'musica mais famosa?',
+    'Cuida bem dela)'
+)
